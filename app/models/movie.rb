2213e2e7567a0914170fde5c52af0cf47eba677f
Sarah Juarez <sarahajuarez11@tamu.edu>
2018-01-22 22:44:16 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
-    attr_accessible :title, :rating, :description, :release_date
+    attr_accessible :title, :rating, :description, :release_date, :director
+
+  # Prefixing with 'self.' makes this a class method
+  def self.get_possible_ratings
+	return %w[G PG PG-13 R]
+  end
 end
