92beaa2b0fbe2dad2ae22b83e392175b32463b68
Sarah Juarez <sarahajuarez11@tamu.edu>
2018-01-22 22:56:35 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,5 @@
 class Movie < ActiveRecord::Base
-    attr_accessible :title, :rating, :description, :release_date, :director
-
-  # Prefixing with 'self.' makes this a class method
-  def self.get_possible_ratings
-	return %w[G PG PG-13 R]
+  def self.ratings
+	self.uniq.pluck(:rating).sort_by
   end
 end
